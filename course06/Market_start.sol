cc7cd0222860232664afeb51c70aaef4c19ad536
supredu <1020665094@qq.com>
2022-11-27 00:05:26 +0800

    nft market

--- course06/Market_start.sol
+++ course06/Market_start.sol
@@ -32,7 +32,9 @@ contract Market is IERC721Receiver {
         uint256 previousPrice,
         uint256 price
     );
-
+// 0x0000000000000000000000000000000000000000000000056BC75E2D63100000 100
+// 0x00000000000000000000000000000000000000000000000AD78EBC5AC6200000 200
+// 0x00000000000000000000000000000000000000000000000821AB0D4414980000 150
     constructor(ERC20 _erc20, ERC721 _erc721) {
         require(
             address(_erc20) != address(0),
@@ -47,17 +49,38 @@ contract Market is IERC721Receiver {
     }
 
     function buy(uint256 _tokenId, uint256 _price) external {
-        // 此处编写业务逻辑
+        Order storage _order=orderOfId[_tokenId];
+        address buyer = msg.sender;
+        address seller = _order.seller;
+        uint256 price = _order.price;
+        require(erc721.ownerOf(_tokenId) == address(this),"NFT not in market" );
+        require(_price>=_order.price,"Increase Price");
+        erc20.transferFrom(buyer,address(this),_price);
+        erc20.transfer(seller,_order.price);
+        erc20.transfer(buyer,_price-_order.price);
+        erc721.safeTransferFrom(address(this),buyer,_tokenId);
+        removeListing(_tokenId);
         emit Deal(buyer, seller, _tokenId, price);
     }
 
     function cancelOrder(uint256 _tokenId) external {
-        // 此处编写业务逻辑
+        Order storage _order = orderOfId[_tokenId];// 此处编写业务逻辑
+        address seller =_order.seller;
+        require (seller == msg.sender,"Not seller");
+        require(erc721.ownerOf(_tokenId) == address(this),"NFT not in market" );
+        erc721.safeTransferFrom(address(this),seller,_tokenId);
+        removeListing(_tokenId);
         emit CancelOrder(seller, _tokenId);
     }
 
     function changePrice(uint256 _tokenId, uint256 _price) external {
-        // 此处编写业务逻辑
+        require(_price>0, "Price must be greater than zero");// 此处编写业务逻辑
+        require(erc721.ownerOf(_tokenId) == address(this),"NFT not in market" );
+        Order storage _order = orderOfId[_tokenId];
+        address seller =_order.seller;
+        require (seller == msg.sender,"Not seller");
+        uint256 previousPrice = _order.price;
+       _order.price=_price;
         emit ChangePrice(seller, _tokenId, previousPrice, _price);
     }
 
@@ -68,7 +91,7 @@ contract Market is IERC721Receiver {
         bytes calldata _data
     ) public override returns (bytes4) {
         // 此处编写业务逻辑
-
+        placeOrder(_seller,_tokenId,toUint256(_data,0));
         return MAGIC_ON_ERC721_RECEIVED;
     }
 
@@ -85,12 +108,24 @@ contract Market is IERC721Receiver {
         uint256 _tokenId,
         uint256 _price
     ) internal {
-        // 此处编写业务逻辑
+        //require(erc721.getApproved(_tokenId) == address(this), "Approval Unset");// 此处编写业务逻辑
+        require(_price>0, "Price must be greater than zero");
+        Order storage _order = orderOfId[_tokenId];
+        _order.seller=_seller;
+        _order.tokenId=_tokenId;
+        _order.price=_price;
+        orders.push(_order);
+        idToOrderIndex[_tokenId]=orders.length-1;
+        //erc721.safeTransferFrom(_seller,address(this),_tokenId);
         emit NewOrder(_seller, _tokenId, _price);
     }
 
-    function removeListing(uint256 _tokenId) internal {
-        // 此处编写业务逻辑
+    function removeListing(uint256 _tokenId) public {
+         idToOrderIndex[orders[orders.length-1].tokenId]=idToOrderIndex[_tokenId];
+         orders[idToOrderIndex[_tokenId]]=orders[orders.length-1];// 此处编写业务逻辑
+         orders.pop();
+         delete orderOfId[_tokenId];
+         delete idToOrderIndex[_tokenId];
     }
 
     // https://stackoverflow.com/questions/63252057/how-to-use-bytestouint-function-in-solidity-the-one-with-assembly
